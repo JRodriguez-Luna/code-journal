2d19bed6fded1012204d981c9aa2e513fecc4186
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-07-16 22:04:31 +0000

    Stores the form's input values in a new object. #14

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
-"use strict";
-/* global data */
-const $photoUrl = document.querySelector('#photo-url');
+'use strict';
+const $entryForm = document.querySelector('form');
+const $photoUrl = document.querySelector('#photoUrl');
 const $previewImg = document.querySelector('#placeholder-img');
-if (!$photoUrl)
-    throw new Error('$photoUrl did not query!');
-if (!$previewImg)
-    throw new Error('$previewImg did not query!');
+if (!$entryForm) throw new Error('$entryForm did not query!');
+if (!$photoUrl) throw new Error('$photoUrl did not query!');
+if (!$previewImg) throw new Error('$previewImg did not query!');
+// set the src attribute of the photo from user input
 $photoUrl.addEventListener('input', (event) => {
-    const url = event.target;
-    $previewImg.setAttribute('src', url.value);
+  const url = event.target;
+  $previewImg.src = url.value;
+});
+$entryForm.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent page from refreshing
+  const $entryElements = $entryForm.elements;
+  // stores the form's input values in a new object
+  const entryData = {
+    title: $entryElements.title.value,
+    photoUrl: $entryElements.photoUrl.value,
+    notes: $entryElements.notes.value,
+  };
+  data.entries.push(entryData);
 });
--- ts/main.ts
+++ ts/main.ts
@@ -1,13 +1,36 @@
 /* global data */
+interface EntryForm extends HTMLFormControlsCollection {
+  title: HTMLInputElement;
+  photoUrl: HTMLInputElement;
+  notes: HTMLTextAreaElement;
+}
 
-const $photoUrl = document.querySelector('#photo-url');
-const $previewImg = document.querySelector('#placeholder-img');
+const $entryForm = document.querySelector('form') as HTMLFormElement;
+const $photoUrl = document.querySelector('#photoUrl') as HTMLInputElement;
+const $previewImg = document.querySelector(
+  '#placeholder-img',
+) as HTMLImageElement;
 
+if (!$entryForm) throw new Error('$entryForm did not query!');
 if (!$photoUrl) throw new Error('$photoUrl did not query!');
 if (!$previewImg) throw new Error('$previewImg did not query!');
 
-
+// set the src attribute of the photo from user input
 $photoUrl.addEventListener('input', (event: Event) => {
   const url = event.target as HTMLInputElement;
-  $previewImg.setAttribute('src', url.value);
+  $previewImg.src = url.value;
+});
+
+$entryForm.addEventListener('submit', (event: Event) => {
+  event.preventDefault(); // prevent page from refreshing
+  const $entryElements = $entryForm.elements as EntryForm;
+
+  // stores the form's input values in a new object
+  const entryData = {
+    title: $entryElements.title.value,
+    photoUrl: $entryElements.photoUrl.value,
+    notes: $entryElements.notes.value,
+  };
+
+  (data.entries as any[]).push(entryData);
 });
