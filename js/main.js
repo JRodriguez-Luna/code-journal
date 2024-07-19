7510e578229ac547bbc64eefeb19472eb2ab9af1
Jesus Rodriguez-Luna <JesusRodriguezLuna3616@gmail.com>
2024-07-19 03:32:14 +0000

    Toggles the no entries text to show or hide when the function is called. #4

--- js/main.js
+++ js/main.js
@@ -2,10 +2,12 @@
 const $entryForm = document.querySelector('#entry-form');
 const $photoUrl = document.querySelector('#photoUrl');
 const $previewImg = document.querySelector('#placeholder-img');
+const $journalEntry = document.querySelector('#journal-entry');
 const placeholderImg = $previewImg.src;
 if (!$entryForm) throw new Error('$entryForm did not query!');
 if (!$photoUrl) throw new Error('$photoUrl did not query!');
 if (!$previewImg) throw new Error('$previewImg did not query!');
+if (!$journalEntry) throw new Error('$journalEntry did not query!');
 // set the src attribute of the photo from user input
 $photoUrl.addEventListener('input', (event) => {
   const url = event.target;
@@ -28,6 +30,10 @@ $entryForm.addEventListener('submit', (event) => {
   // adds to the beginning of entries array
   data.entries.unshift(entryData);
   writeData();
+  toggleNoEntries();
+  // Add new entry to the DOM
+  const $li = renderEntry(entryData);
+  $journalEntry.prepend($li);
   // reset preview img
   $previewImg.src = placeholderImg;
   $entryForm.reset(); // reset form
@@ -38,30 +44,37 @@ function renderEntry(entry) {
   const $columnImg = document.createElement('div');
   const $columnText = document.createElement('div');
   const $img = document.createElement('img');
-  const $heading = document.createElement('h2');
+  const $entryTitle = document.createElement('h2');
   const $description = document.createElement('p');
   $row.setAttribute('class', 'row');
   $columnImg.setAttribute('class', 'column-half');
   $columnText.setAttribute('class', 'column-half');
-  $heading.setAttribute('class', 'header-font');
+  $entryTitle.setAttribute('class', 'header-font');
   // entry data
   $img.src = entry.photoUrl;
   $img.alt = entry.title;
-  $heading.textContent = entry.title;
+  $entryTitle.textContent = entry.title;
   $description.textContent = entry.notes;
   $columnImg.appendChild($img);
-  $columnText.append($heading, $description);
+  $columnText.append($entryTitle, $description);
   $row.append($columnImg, $columnText);
   $li.appendChild($row);
   return $li;
 }
 //  load entries onto webpage
 document.addEventListener('DOMContentLoaded', () => {
-  const $ul = document.querySelector('#journal-entry');
-  if (!$ul) throw new Error('$ul did not query!');
+  if (!$journalEntry) throw new Error('$journalEntry did not query!');
+  toggleNoEntries();
   // loop through the data.entries array
   for (let i = 0; i < data.entries.length; i++) {
     const $newListItem = renderEntry(data.entries[i]);
-    $ul.appendChild($newListItem);
+    $journalEntry.appendChild($newListItem);
   }
 });
+// toggles the no entries text to show or hide when the function is called
+const toggleNoEntries = () => {
+  const $noEntryText = document.querySelector('.no-entries-text');
+  if (!$noEntryText) throw new Error('$noEntryText failed to query!');
+  if (data.entries.length) $noEntryText.classList.add('hidden');
+  else $noEntryText.classList.remove('hidden');
+};
